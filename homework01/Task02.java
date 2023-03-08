a35d430909795e282f1066cd104e2a8bf8e146d0
[Yulia Lebedeva] <yl.75@mail.ru>
2023-03-08 12:15:21 +0500

    task 02 is done

---
+++ homework01/Task02.java
@@ -0,0 +1,21 @@
+
+// Вывести все простые числа от 1 до 1000
+
+public class Task02 {
+   public static void main(String[] args) {
+    boolean flag = true;    
+    for (int number = 2; number <= 1000; number++) {
+        for (int divider = 2; divider < number; divider++) {
+            if (number % divider == 0){
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true)
+        System.out.print(number + " ");
+        else flag = true;
+        }
+    } 
+}
+
+
