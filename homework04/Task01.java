7ef53b71b69ff0a1f7b52246569ee3e17c2bf2ef
[Yulia Lebedeva] <yl.75@mail.ru>
2023-03-19 18:33:12 +0500

    add second solving of the task

--- homework04/Task01.java
+++ homework04/Task01.java
@@ -6,7 +6,7 @@
 package homework04;
 
 import java.util.LinkedList;
-
+import java.util.Random;
 
 public class Task01 {
     public static void main(String[] args) {
@@ -28,9 +28,40 @@ public class Task01 {
 
     static LinkedList<Integer> reverseList(LinkedList<Integer> linkedList) {
         LinkedList<Integer> newList = new LinkedList<>();
-        while (linkedList.size() > 0 ) {
+        while (linkedList.size() > 0) {
             newList.add(linkedList.pollLast());
         }
         return newList;
+    }
+}
+
+// второй вариант решения задачи
+
+class Task01a {
+    public static void main(String[] args) {
+        LinkedList<Integer> lst = new LinkedList<>();
+        rand_add_list(10, 100, lst);
+        System.out.println("Исходный список:     " + lst);
+
+        LinkedList<Integer> reverse_lst = new LinkedList<>();
+        reverse_list(lst, reverse_lst);
+        System.out.println("Перевернутый список: " + reverse_lst);
+
+    }
+
+    public static void rand_add_list(int count, int maxValue, LinkedList<Integer> list) {
+        Random rand = new Random();
+        for (int i = 0; i < count; i++) {
+            list.add(rand.nextInt(maxValue));
+        }
+    }
+
+    public static LinkedList<Integer> reverse_list(LinkedList<Integer> list, LinkedList<Integer> new_list) {
+        for (int i = 0; i < list.size(); i++) {
+            new_list.add(list.peekLast());
+            list.removeLast();
+            i--;
+        }
+        return new_list;
     }
 }
